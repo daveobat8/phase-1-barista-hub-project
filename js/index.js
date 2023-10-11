96c17b6806508f60ea48b4a85d97f674187d49fa
David <dave.obat@student.moringashool.com>
2023-10-11 11:43:29 +0300

    content displayed on browser

--- js/index.js
+++ js/index.js
@@ -1,23 +1,50 @@
-const BASE_URL = 'https://api.api-ninjas.com/v1/cocktail?name=rum'
+const BASE_URL = 'https://api.api-ninjas.com/v1/cocktail?name=cola';
+
 //ensure DOM is loaded
 
 document.addEventListener('DOMContentLoaded', () => {
-  getFirstFilm()
+  getCocktails()
+
+
 });
-// fetch first film data and display on DOM
-let headersList = {
+
+
+// create a headerlist 
+const headersList = {
     "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    //"User-Agent": "Thunder Client (https://www.thunderclient.com)",
     "X-Api-Key": "1ivn3UXAQv0kdbLKoqS2ag==0JFIXAWueVMxtr47"
-   }
-function getFirstFilm (){
+   };
+
+   // fetch details
+
+
+function getCocktails (){
 
     fetch( BASE_URL, {
         method: 'GET',
         headers: headersList,
          }).then((res) => res.json())
-           .then((data) => console.log(data))
+           .then(renderAllCocktails)
             .catch((err) => {
         console.log(err)
     });
 }; 
+
+
+function renderAllCocktails(cocktails){
+  const menu= document.querySelector('.drinks_details')
+
+  
+cocktails.forEach((cocktail) => {
+      const cocktailName= document.createElement('h1')
+      cocktailName.textContent= cocktail.name
+       cocktailName.addEventListener ('click',() => {
+        console.log(`clicked ${cocktail.name}`)
+       }) 
+      menu.appendChild(cocktailName);
+      
+  })
+};
+
+
